348fe41e195f0951869ee027265cb01f69a93488
AbdiN7 <abdirahman.noor@smoothstack.com>
2019-11-19 18:07:50 -0500

    Added Routing for the application
    added 3 components Home BookingList appBarHeader. Also added dependencies that were added in the other branches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import {Switch, Route} from 'react-router-dom';
+import home from './components/home';
+import BookingList from './components/BookingList'
+import AppBarHeader from './components/appBarHeader'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        HELLO 2
-      </header>
+      <AppBarHeader/>
+      <Switch>
+        <Route exact path='/' component={home}/>
+        <Route path='/booking' component={BookingList}/>
+      </Switch>
     </div>
+
   );
 }
 
---
+++ src/components/BookingList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BookingList = () => {
+    return (
+        <div>
+          Bookings
+        </div>
+    )
+}
+export default BookingList;
