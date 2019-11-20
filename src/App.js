52c5dae08129cbae69a5d141caa69d6c5987c137
Tim Johnson <tim.johnson@smoothstack.com>
2019-11-20 14:40:55 -0500

    added signup page temp default template

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import {Switch, Route} from 'react-router-dom';
 import home from './components/home';
-import BookingList from './components/BookingList'
-import AppBarHeader from './components/appBarHeader'
+import BookingList from './components/BookingList';
+import AppBarHeader from './components/appBarHeader';
+import SignUp from './components/SignUp';
+
 function App() {
   return (
     <div className="App">
       <AppBarHeader/>
       <Switch>
         <Route exact path='/' component={home}/>
+        <Route path='/signup' component={SignUp}/>
         <Route path='/booking' component={BookingList}/>
       </Switch>
     </div>
